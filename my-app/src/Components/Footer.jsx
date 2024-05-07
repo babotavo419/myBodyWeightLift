688768b42e3868d8c91908683a090fdf11f5ef4d
babotavo419 <one.faith419@gmail.com>
2024-05-07 14:37:01 -0400

    new start

---
+++ my-app/src/Components/Footer.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faFacebookF,
+    faXTwitter,
+    faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+} from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+
+const Footer = () => {
+    return (
+        <footer className="">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className='ml-8 py-5 space-y-3'>
+                <h2 className="font-semibold">Contact Us</h2>
+                    <p className="font-bold py-3 text-slate-800">The Rats Den</p>
+                    <p className=" text-gray-800">1488 Back Ally LN</p>
+                    <p className=" text-gray-800">Toledo, Ohio</p>
+                    <p className=" text-gray-800">Open M-F: 9:00 AM – 5:00 PM</p>
+                    <p className=" text-gray-800">Phone: (419)867-5309</p>
+                    <div className="flex space-x-3">
+                <a className="flex items-center justify-center" href="https://twitter.com/octavio31848">
+                    <FontAwesomeIcon icon={faFacebookF} className="h-5 w-5 text-gray-600 dark:text-gray-300" />
+                </a>
+                <a className="flex items-center justify-center ml-4" href="https://www.linkedin.com/in/octaviovsanchez/">
+                    <FontAwesomeIcon icon={faXTwitter} className="h-5 w-5 text-gray-600 dark:text-gray-300" />
+                </a>
+                <a className="flex items-center justify-center ml-4" href="https://www.linkedin.com/in/octaviovsanchez/">
+                    <FontAwesomeIcon icon={faYoutube} className="h-5 w-5 text-gray-600 dark:text-gray-300" />
+                </a>
+                </div>
+                </div>
+
+                <div className="ml-8 py-5 space-y-3">
+                    <div className="w-full sm:w-1/2 md:w-1/4">
+                    <h5 className="font-bold py-3 text-slate-800">
+                            Help
+                        </h5>
+                        <p className="text-gray-800">
+                            <NavLink
+                                to="/custom-logo"
+                                className="hover:underline">
+                                Workout Plans
+                            </NavLink>
+                        </p>
+                        <p className="text-gray-800">
+                            <NavLink
+                                to="/custom-logo"
+                                className="hover:underline">
+                                Bolg
+                            </NavLink>
+                        </p>
+                    </div>
+
+                    <div className="w-full sm:w-1/2 md:w-1/4">
+                        <p className="text-gray-800">
+                            <NavLink
+                                to="/returns"
+                                className="hover:underline">
+                                Return Policy
+                            </NavLink>
+                        </p>
+                        <p className="text-gray-800">
+                            <NavLink
+                                to="/contact"
+                                className="hover:underline">
+                                Contact
+                            </NavLink>
+                        </p>
+                        <p className="text-gray-800">
+                            <NavLink
+                                to="/refund"
+                                className="hover:underline">
+                                Refund Policy
+                            </NavLink>
+                        </p>
+                        <p className="text-gray-800">
+                            <NavLink
+                                to="/privacy"
+                                className="hover:underline">
+                                Privacy Policy</NavLink></p>
+                        <p className="text-gray-800">
+                            <NavLink
+                                to="/terms"
+                                className="hover:underline">
+                                Terms & Conditions
+                            </NavLink>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="text-center py-4 mt-4">
+                <a className="ml-4 text-sm text-gray-500 dark:text-gray-400" href="https://www.octavioweb.dev/">© 2024 octavioweb.dev </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- my-app/src/LandingPage.jsx
+++ my-app/src/LandingPage.jsx
@@ -2,35 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDumbbell, faWeightScale, faScaleBalanced } from '@fortawesome/free-solid-svg-icons';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 
 function LandingPage() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="w-full px-6 py-4 bg-white dark:bg-gray-800">
-        <nav className="flex items-center justify-between">
-          <Link to="/weight-equalizer" className="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl" href="#">
-            Body & Weight Equalizer
-          </Link>
-          <div className="space-x-4">
-          <Link to="/" className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300" href="#">
-              Home
-            </Link>
-            <Link to="/weight-equalizer" className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300" href="#">
-              BWE App
-            </Link>
-            <Link to="/calorie-counter" className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300" href="#">
-              Calorie Counter
-            </Link>
-            <Link to="/workouttracker" className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300" href="#">
-              Workout Tracker
-            </Link>
-          </div>
-        </nav>
-      </header>
+    <div className="flex flex-col">
+      <Header />
       <main className="flex flex-col items-center justify-center flex-grow text-center px-6">
         <h1 className="text-3xl font-semibold text-gray-800 dark:text-white sm:text-4xl">Welcome to the Body & Weight Equalizer App</h1>
-        <h2 className="mt-2 text-sm text-gray-500 dark:text-gray-400 sm:text-base">
+        <h2 className="mt-6 text-sm text-gray-500 dark:text-gray-400 sm:text-base">
         The "Weight Equalizer" app is designed as a fitness planning tool to 
         help users align their body weight and strength training goals, particularly 
         focusing on progressive weight. Key Functionalities and Logic include state management and dynamic rendering, 
@@ -39,9 +21,9 @@ function LandingPage() {
         to the chosen goal. It includes error handling to guide users towards realistic goal-setting 
         and a results section that displays the calculated outcome.
         </h2>
-        <Link to="/weight-equalizer" className="mt-5 px-5 py-2 text-sm font-medium text-white bg-gray-800 rounded-lg">Get Started</Link>
+        <Link to="/weight-equalizer" className="mt-5 mb-6 px-5 py-2 text-sm font-medium text-white bg-gray-800 rounded-lg">Get Started</Link>
       </main>
-      <section className="flex flex-col items-center justify-center px-6 py-10 bg-gray-100 dark:bg-gray-900">
+      <section className="flex flex-col mb-0 items-center justify-center px-6 py-10 bg-gray-100 dark:bg-gray-900">
         <div className="grid gap-6 mb-8 md:grid-cols-3">
           <div className="flex flex-col items-center justify-center p-4 text-center bg-white rounded-lg dark:bg-gray-800">
           <div className="p-3 text-3xl text-gray-800 dark:text-white">
@@ -75,61 +57,10 @@ function LandingPage() {
           </div>
         </div>
       </section>
-      <footer className="flex items-center justify-center w-full h-12 border-t dark:border-gray-700">
-        <a className="flex items-center justify-center" href="https://twitter.com/octavio31848">
-          <TwitterIcon className="h-5 w-5 text-gray-600 dark:text-gray-300" />
-        </a>
-        <a className="flex items-center justify-center ml-4" href="https://www.linkedin.com/in/octaviovsanchez/">
-          <LinkedinIcon className="h-5 w-5 text-gray-600 dark:text-gray-300" />
-        </a>
-        <a className="ml-4 text-sm text-gray-500 dark:text-gray-400" href="https://www.octavioweb.dev/">© 2024 octavioweb.dev </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
-
-
-function LinkedinIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-      <rect width="4" height="12" x="2" y="9" />
-      <circle cx="4" cy="4" r="2" />
-    </svg>
-  )
-}
-
-
-function TwitterIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
-    </svg>
-  )
-}
-
 
 export default LandingPage;
 
